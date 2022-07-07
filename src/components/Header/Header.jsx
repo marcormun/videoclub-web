fa7ef9e1a4b5629fcedd855484a4c3f448ccf1fc
marcormun <mcordontrabajo@gmail.com>
2022-07-07 14:10:38 +0200

    fix: logica header mejorada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,60 +16,44 @@ const Header = () => {
     const viajar = (destino) => {
         navegador(destino)
     };
-    if (!credenciales?.user_role) {
-        return (
-            <div className="font-sans bg-gray-900 text-white">
+    return(
+        <div className="font-sans bg-gray-900 text-white">
                 <nav className="border-b border-gray-800">
                     <div className="container mx-auto flex items-end justify-between px-4 py-6">
+                        <div className='cursor-pointer' onClick={() => viajar("/")}>
+                            <ul className="flex items-center" >
+                                <li>
+                                    <FilmIcon className="h-6 w-6 text-white" />
+                                </li>
+                                <li>
+                                    <div className="text-white text-2xl font-bold">&nbsp;FilmClub</div>
+                                </li>
+                            </ul>
+                        </div>
+                        
+                        {!credenciales.user_role ?
+                            <ul className="flex items-center">
+                                <li className='ml-16'>
+                                    <div className="headerButton" onClick={() => viajar("/login")}>Login</div>
+                                </li>
+                                <li className='ml-16'>
+                                    <div className='textLink' onClick={() => viajar("/register")}>Register</div>
+                                </li>
+                            </ul>
+                        :
                         <ul className="flex items-center">
-                            <li>
-                                <FilmIcon className="h-6 w-6 text-white" />
-                            </li>
-                            <li>
-                                <a href="" className="text-white text-2xl font-bold">&nbsp;FilmClub</a>
-                            </li>
-                        </ul>
-                        <ul className="flex items-center">
-                            <li className='ml-16'>
-                                <div className="headerButton" onClick={() => viajar("/login")}>Login</div>
-                            </li>
-                            <li className='ml-16'>
-                                <div className='textLink' onClick={() => viajar("/register")}>Register</div>
-                            </li>
-                        </ul>
+                        <li className='ml-16'>
+                            <div className='textLink' onClick={() => viajar("/profile")}>Perfil</div>
+                        </li>
+                        <li className='ml-16'>
+                            <div className='textLink' onClick={() => dispatch(logOut())}>Cerrar sesión</div>
+                        </li>
+                    </ul>
+                    }
                     </div>
                 </nav>
             </div>
-        )
-    } else {
-
-        return (
-            <div className="font-sans bg-gray-900 text-white">
-                <nav className="border-b border-gray-800">
-                    <div className="container mx-auto flex items-end justify-between px-4 py-6">
-                        <ul className="flex items-center">
-                            <li>
-                                <FilmIcon className="h-6 w-6 text-white" />
-                            </li>
-                            <li>
-                                <a href="" className="text-white text-2xl font-bold">&nbsp;FilmClub</a>
-                            </li>
-                        </ul>
-                        <ul className="flex items-center">
-                            <li className='ml-16'>
-                                <div className='textLink' onClick={() => viajar("/profile")}>{credenciales?.user_role}</div>
-                            </li>
-                            <li className='ml-16'>
-                                <div className='textLink' onClick={() => dispatch(logOut())}>log out</div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-
-        )
-
-    }
+    )
 };
 
 export default Header;
