36421313a102c65f60113fa0887200fbf97278c9
Santiago Dionis <santi.dionis@gmail.comn>
2022-07-09 16:33:20 +0200

    profile test update and delete 2

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -5,13 +5,31 @@ import { userData } from '../userSlice';
 import jwt from 'jwt-decode';
 
 const Delete = () => {
-    axios.delete("https://videoclub-proyecto5.herokuapp.com/api/users", {headers:{id: userData.user_mail}}    );
-    console.log(userData);
+    axios.delete("https://videoclub-proyecto5.herokuapp.com/api/users", {
+
+        headers: {
+            Authorization: `Beared ${userData.token}`,
+            datos_perfil: userData.user_id
+
+        }
+    });
+
 };
+
+
 const Update = () => {
-    axios.put("https://videoclub-proyecto5.herokuapp.com/api/users", {headers:{id: userData.user_mail}}    );
-    console.log(userData);
-    console.log("update");
+    axios.get("https://videoclub-proyecto5.herokuapp.com/api/users", {
+        headers: {
+            Authorization: `Beared ${userData.token}`,
+            userData: userData.user_id
+        }
+    }
+    );
+
+
+
+
+
 };
 
 
@@ -41,10 +59,10 @@ const Profile = () => {
                 <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Telefono</label>
                 <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_phone}</p>
 
-                <div className="content-center h-12 w-64 mt-6 bg-red-700 hover:bg-green-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={()=>Delete()}>Dar de Baja</div>
-            
-                <div className="content-center h-12 w-64 mt-6 bg-blue-700 hover:bg-blue-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={()=>Update()}>Update</div>
-            
+                <div className="content-center h-12 w-64 mt-6 bg-red-700 hover:bg-green-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={() => Delete()}>Dar de Baja</div>
+
+                <div className="content-center h-12 w-64 mt-6 bg-blue-700 hover:bg-blue-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={() => Update()}>Update</div>
+
             </div>
 
         </div>
