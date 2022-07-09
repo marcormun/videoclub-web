62fe70e8a5a6b81d3853843216d0f9a671fece48
Santiago Dionis <santi.dionis@gmail.comn>
2022-07-09 15:01:39 +0200

    profile test update and delete

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { userData } from '../userSlice';
 import jwt from 'jwt-decode';
+
+const Delete = () => {
+    axios.delete("https://videoclub-proyecto5.herokuapp.com/api/users", {headers:{id: userData.user_mail}}    );
+    console.log(userData);
+};
+const Update = () => {
+    axios.put("https://videoclub-proyecto5.herokuapp.com/api/users", {headers:{id: userData.user_mail}}    );
+    console.log(userData);
+    console.log("update");
+};
+
 
 
 const Profile = () => {
@@ -24,6 +35,16 @@ const Profile = () => {
                 <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_email}</p>
                 <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Role</label>
                 <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_role}</p>
+
+                <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Direccion</label>
+                <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_address}</p>
+                <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Telefono</label>
+                <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_phone}</p>
+
+                <div className="content-center h-12 w-64 mt-6 bg-red-700 hover:bg-green-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={()=>Delete()}>Dar de Baja</div>
+            
+                <div className="content-center h-12 w-64 mt-6 bg-blue-700 hover:bg-blue-800 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={()=>Update()}>Update</div>
+            
             </div>
 
         </div>
