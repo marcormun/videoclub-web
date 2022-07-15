ae2d48c9232956c94f3f211125a2aeb2f011e8e8
Santiago Dionis <santi.dionis@gmail.comn>
2022-07-15 17:58:30 +0200

    test

--- src/containers/User/Update/Update.jsx
+++ src/containers/User/Update/Update.jsx
@@ -7,6 +7,8 @@ import { updateUser, userData } from '../userSlice';
 
 
 
+
+
 const Editprofile = () => {
 
     //Me genero una variable que va a LEER de Redux
@@ -14,9 +16,11 @@ const Editprofile = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+
     //Hocks
+
     const [perfilUsuario, setPerfilUsuario] = useState({
-        user_name:datosUsuario.user_name,
+        user_name: datosUsuario.user_name,
         user_surname: datosUsuario.user_surname,
         user_email: datosUsuario.user_email,
         user_address: datosUsuario.user_address,
@@ -24,9 +28,16 @@ const Editprofile = () => {
         user_mobile: datosUsuario.user_mobile,
         user_token: datosUsuario.user_token
 
-    }) 
-    
-    
+    })
+  
+
+    //handkeys
+
+    const handlerInputs = (e) => {
+        setPerfilUsuario({ ...perfilUsuario, [e.target.name]: e.target.value })
+
+    }
+
 
 
     return (
@@ -35,32 +46,32 @@ const Editprofile = () => {
 
 
 
-        <div class="grid grid-cols-3 gap-9 bg-size-auto ">
+        <div class="grid grid-cols-2 gap-4 bg-size-auto ">
             <pre>{JSON.stringify(datosUsuario, null,2)}</pre>
 
+
+
             <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Nombre</label>
-            <p className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_name}</p>
-            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"></input>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4" value={perfilUsuario.user_name} name='user_name' title='name' onChange={handlerInputs}></input>
+
+            <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Apellido</label>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3" value={perfilUsuario.user_surname} name='user_surname' title='surname' onChange={handlerInputs}></input>
 
             <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Email</label>
-            <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_email}</p>
-            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"></input>
-
-            <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Role</label>
-            <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_role}</p>
-            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"></input>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"value={perfilUsuario.user_email} name='user_email' title='email' disabled onChange={handlerInputs}></input>
 
             <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Direccion</label>
-            <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_address}</p>
-            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"></input>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"value={perfilUsuario.user_address} name='user_address' title='address' onChange={handlerInputs}></input>
+
+            <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Localidad</label>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"value={perfilUsuario.user_city} name='user_city' title='city' onChange={handlerInputs}></input>
 
             <label className="block text-gray-500 font-bold md:text-top mt-3 mb-3 pr-4">Telefono</label>
-            <p className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4">{datosUsuario.user_phone}</p>
-            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"></input>
+            <input type="text" className="block text-blue-500 font-bold md:text-top mt-3 mb-3 pr-4"value={perfilUsuario.user_mobile} name='user_mobile' title='mobile' onchanges={handlerInputs}></input>
 
             <div class="flex  gap-9 items-center justify-center">
 
-                <div className="block   h-14 w-64 mt-6 bg-blue-700 hover:bg-blue-900 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(updateUser(datosUsuario,perfilUsuario),navigate('/'))}>Guardar</div>
+                <div className="block   h-14 w-64 mt-6 bg-blue-700 hover:bg-blue-900 cursor-pointer text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(updateUser(datosUsuario, perfilUsuario), navigate('/'))}>Guardar</div>
             </div>
 
         </div>
