46be8cda9245ef1a7895c5b714ad8617ba1362fc
marcormun <mcordontrabajo@gmail.com>
2022-07-18 10:04:43 +0200

    fix: Logica filmdetail

--- src/containers/FilmDetail/FilmDetail.jsx
+++ src/containers/FilmDetail/FilmDetail.jsx
@@ -77,11 +77,12 @@ const FilmDetail = () => {
                     <div className='mt-12 flex'>
                         <button type="button" onClick={()=> alquilar()} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Alquilar</button>
                     </div>
-                    {alquiler==true ?
+                    {alquiler==true &&
                         <div className="mt-12 flex text-lime-400">
                             Alquiler realizado correctamente
                         </div>
-                    :
+                    }
+                    {alquiler==false &&
                         <div className="mt-12 flex text-red-600">
                             Ya tienes una pelicula alquilada
                         </div>
